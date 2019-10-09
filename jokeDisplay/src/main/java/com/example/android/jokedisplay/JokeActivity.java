d8c22898663e0de5d1290212d5afaf008b187959
Dan C <dcronin202@gmail.com>
2019-10-09 11:47:42 -0500

    Created an Android library containing an Activity that displays a joke passed to it as an intent extra

---
+++ jokeDisplay/src/main/java/com/example/android/jokedisplay/JokeActivity.java
@@ -0,0 +1,19 @@
+package com.example.android.jokedisplay;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+
+public class JokeActivity extends AppCompatActivity {
+
+    public static String JOKE_KEY = "Joke key";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_joke);
+
+    }
+
+
+}
