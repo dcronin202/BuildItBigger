b075c6f7e2f377bba23f6e00cb1ca4d170ba30b0
Dan C <dcronin202@gmail.com>
2019-10-11 11:29:39 -0500

    Added an AsyncTask to retrieve jokes from the Google Cloud Endpoint server

--- app/src/main/java/com/udacity/gradle/builditbigger/MainActivity.java
+++ app/src/main/java/com/udacity/gradle/builditbigger/MainActivity.java
@@ -1,24 +1,37 @@
 package com.udacity.gradle.builditbigger;
 
-import android.content.Context;
+import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Pair;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+
+import com.example.android.jokedisplay.JokeActivity;
+import com.google.api.client.extensions.android.http.AndroidHttp;
+import com.google.api.client.extensions.android.json.AndroidJsonFactory;
+import com.google.api.client.googleapis.services.AbstractGoogleClientRequest;
+import com.google.api.client.googleapis.services.GoogleClientRequestInitializer;
+import com.udacity.gradle.builditbigger.backend.myApi.MyApi;
+
+import java.io.IOException;
 
 
 public class MainActivity extends AppCompatActivity {
+
+    Button button;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        new EndpointsAsyncTask().execute(new Pair<Context, String>(this, "Tell me a joke!"));
+        new MainEndpointsAsyncTask().execute();
 
     }
-
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -42,10 +55,56 @@ public class MainActivity extends AppCompatActivity {
         return super.onOptionsItemSelected(item);
     }
 
-    // Step 1
+    // STEP 1: Create a Java Library
     /* public void tellJoke(View view) {
         JavaJokes jokes = new JavaJokes();
-        Toast.makeText(this, jokes.fetchJavaJoke(), Toast.LENGTH_SHORT).show();
+        Toast.makeText(this, jokes.fetchJoke(), Toast.LENGTH_SHORT).show();
     } */
+
+    // STEP 3: Setup GCE
+    class MainEndpointsAsyncTask extends AsyncTask<Void, Void, String> {
+        private MyApi myApiService = null;
+
+
+        @Override
+        protected String doInBackground(Void... voids) {
+
+            if(myApiService == null) {
+                MyApi.Builder builder = new MyApi.Builder(AndroidHttp.newCompatibleTransport(),
+                        new AndroidJsonFactory(), null)
+                        .setRootUrl("http://192.168.1.4:8080/_ah/api/")
+                        .setGoogleClientRequestInitializer(new GoogleClientRequestInitializer() {
+                            @Override
+                            public void initialize(AbstractGoogleClientRequest<?> abstractGoogleClientRequest) throws IOException {
+                                abstractGoogleClientRequest.setDisableGZipContent(true);
+                            }
+                        });
+
+                myApiService = builder.build();
+            }
+
+            try {
+                return myApiService.getJoke().execute().getData();
+
+            } catch (IOException e) {
+                return e.getMessage();
+
+            }
+        }
+
+        @Override
+        protected void onPostExecute(final String result) {
+            //Toast.makeText(MainActivity.this, result, Toast.LENGTH_LONG).show();
+            button = findViewById(R.id.joke_button);
+            button.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    Intent intent = new Intent(view.getContext(), JokeActivity.class);
+                    intent.putExtra(JokeActivity.JOKE_KEY, result);
+                    startActivity(intent);
+                }
+            });
+        }
+    }
 
 }
