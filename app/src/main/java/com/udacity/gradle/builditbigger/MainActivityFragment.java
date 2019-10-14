860679bd739812bf02d26b5724e432926f680bc2
Dan C <dcronin202@gmail.com>
2019-10-14 15:25:27 -0500

    Created a MainActivityFragement file for the free version and moved the code for the AdView from the main MainActivityFragment to the free version's MainActivityFragment

--- app/src/main/java/com/udacity/gradle/builditbigger/MainActivityFragment.java
+++ app/src/main/java/com/udacity/gradle/builditbigger/MainActivityFragment.java
@@ -8,8 +8,6 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 
-import com.google.android.gms.ads.AdRequest;
-import com.google.android.gms.ads.AdView;
 
 /**
  * A placeholder fragment containing a simple view.
@@ -30,14 +28,6 @@ public class MainActivityFragment extends Fragment {
 
         //setButton(root);
 
-        AdView mAdView = (AdView) root.findViewById(R.id.adView);
-        // Create an ad request. Check logcat output for the hashed device ID to
-        // get test ads on a physical device. e.g.
-        // "Use AdRequest.Builder.addTestDevice("ABCDEF012345") to get test ads on this device."
-        AdRequest adRequest = new AdRequest.Builder()
-                .addTestDevice(AdRequest.DEVICE_ID_EMULATOR)
-                .build();
-        mAdView.loadAd(adRequest);
         return root;
     }
 
