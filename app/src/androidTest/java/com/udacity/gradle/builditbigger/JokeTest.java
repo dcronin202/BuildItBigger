9f3ad71a8863c890aebd534aad707e37c7364ce9
Dan C <dcronin202@gmail.com>
2019-10-14 15:22:37 -0500

    Removed unneeded code

--- app/src/androidTest/java/com/udacity/gradle/builditbigger/JokeTest.java
+++ app/src/androidTest/java/com/udacity/gradle/builditbigger/JokeTest.java
@@ -3,13 +3,7 @@ package com.udacity.gradle.builditbigger;
 import android.support.test.espresso.ViewInteraction;
 import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
-import android.view.View;
-import android.view.ViewGroup;
-import android.view.ViewParent;
 
-import org.hamcrest.Description;
-import org.hamcrest.Matcher;
-import org.hamcrest.TypeSafeMatcher;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -38,9 +32,8 @@ public class JokeTest {
 
         // Find the view
         ViewInteraction appCompatButton = onView(
-        allOf(withId(R.id.joke_button),
-        childAtPosition(childAtPosition(withId(R.id.fragment),
-        0),1),isDisplayed()));
+                allOf(withId(R.id.joke_button),
+                isDisplayed()));
 
         // Perform action on the view
         appCompatButton.perform(click());
@@ -48,25 +41,6 @@ public class JokeTest {
         // Check to make sure a joke is displayed
         onView(withId(R.id.joke_textview)).check(matches(isDisplayed()));
 
-    }
-
-    private static Matcher<View> childAtPosition(
-            final Matcher<View> parentMatcher, final int position) {
-
-        return new TypeSafeMatcher<View>() {
-            @Override
-            public void describeTo(Description description) {
-                description.appendText("Child at position " + position + " in parent ");
-                parentMatcher.describeTo(description);
-            }
-
-            @Override
-            public boolean matchesSafely(View view) {
-                ViewParent parent = view.getParent();
-                return parent instanceof ViewGroup && parentMatcher.matches(parent)
-                        && view.equals(((ViewGroup) parent).getChildAt(position));
-            }
-        };
     }
 
 }
